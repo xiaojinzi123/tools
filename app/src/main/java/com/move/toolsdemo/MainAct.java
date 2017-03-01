f4c5f3e51a1a82932bdea8e3aaca5b93a8fed366
xiaojinzi <347837667@qq.com>
2017-03-01 19:31:49 +0800

    随便写了点demo用法

--- app/src/main/java/com/move/toolsdemo/MainAct.java
+++ app/src/main/java/com/move/toolsdemo/MainAct.java
@@ -3,11 +3,18 @@ package com.move.toolsdemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.move.tools.android.L;
+import com.move.tools.android.T;
+
 public class MainAct extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_main);
+
+        T.showShort(this,"提示");
+        L.s("tag","提示");
+
     }
 }
